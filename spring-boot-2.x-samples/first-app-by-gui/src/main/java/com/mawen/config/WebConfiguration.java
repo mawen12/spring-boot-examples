aad167456437465f69c2ffe213e5001fe73d8647
mawen12 <1181963012mw@gmail.com>
2022-12-12 18:20:32 +0800

    自动装配

---
+++ spring-boot-2.x-samples/first-app-by-gui/src/main/java/com/mawen/config/WebConfiguration.java
@@ -0,0 +1,43 @@
+package com.mawen.config;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.context.WebServerInitializedEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+@Configuration
+//@SpringBootApplication
+//@EnableAutoConfiguration
+public class WebConfiguration {
+
+    @Bean
+    public RouterFunction<ServerResponse> helloWorld() {
+        return route(GET("/hello-world"),
+                request -> ok().body(Mono.just("Hello,World"), String.class)
+        );
+    }
+
+    @Bean
+    public ApplicationRunner runner(BeanFactory beanFactory) {
+        return args -> {
+            System.out.println("当前 helloWorld Bean 实现类为：" + beanFactory.getBean("helloWorld").getClass().getName());
+            System.out.println("当前 WebConfiguration Bean 实现类为：" + beanFactory.getBean(WebConfiguration.class).getClass().getName());
+        };
+    }
+
+    @EventListener(WebServerInitializedEvent.class)
+    public void onWebServerReady(WebServerInitializedEvent event) {
+        System.out.println("当前 WebServer 实现类为：" + event.getWebServer().getClass().getName());
+    }
+}
