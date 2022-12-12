864e86695fdbb94bcbb28669a0c3ada2e4a36f2c
mawen12 <1181963012mw@gmail.com>
2022-12-12 21:27:13 +0800

    Production-ready

--- spring-boot-2.x-samples/first-app-by-gui/src/main/java/com/mawen/config/WebAutoConfiguration.java
+++ spring-boot-2.x-samples/first-app-by-gui/src/main/java/com/mawen/config/WebAutoConfiguration.java
@@ -1,8 +1,10 @@
 package com.mawen.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+@ConditionalOnWebApplication
 @Configuration
 @Import(WebConfiguration.class)
 public class WebAutoConfiguration {
