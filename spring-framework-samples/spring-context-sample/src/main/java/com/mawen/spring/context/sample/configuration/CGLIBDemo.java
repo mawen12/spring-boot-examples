42222e8c51b3f433c7ced9f425e5ffa9c4c1bbbe
mawen12 <1181963012mw@gmail.com>
2023-01-14 02:12:01 +0800

    CGLIB

--- spring-framework-samples/spring-context-sample/src/main/java/com/mawen/spring/context/sample/configuration/CGLIBDemo.java
+++ spring-framework-samples/spring-context-sample/src/main/java/com/mawen/spring/context/sample/configuration/CGLIBDemo.java
@@ -1,48 +1,52 @@
 package com.mawen.spring.context.sample.configuration;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.stream.Stream;
 
 /**
- * {@link Configuration}示例
+ * CGLIB控制{@link Configuration}类
  * <p>
- * {@link Configuration}标注的类在启动时，会使用CGLIB进行子类化，生成的子类名称：ConfigExample$EnhancerBySpringCGLIB$$xxxx
- * <p>
+ * Spring将使用{@link Bean}注解的类缓存起来，下次调用时，直接返回类的实例
  *
  * @author <a href="1181963012mw@gmail.com">mawen12</a>
  * @since 2023/1/13
  */
 @Configuration
-public class ConfigExample {
+public class CGLIBDemo {
+
+    private int counter;
 
     @Bean
-    public Greeter greeter() {
-        return new Greeter();
+    @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public String something() {
+//        printInfo(this);
+        System.out.println("");
+        System.out.println("Stack Trace: ");
+        Stream.of(Thread.currentThread().getStackTrace()).forEach(System.out::println);
+        System.out.println("");
+        System.out.println("method invoked");
+        return String.valueOf(++counter);
     }
 
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
-                ConfigExample.class);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(CGLIBDemo.class);
 
-        ConfigExample configExample = context.getBean(ConfigExample.class);
-        printInfo(configExample);
+        CGLIBDemo bean = context.getBean(CGLIBDemo.class);
 
-        Greeter greeter = context.getBean(Greeter.class);
-        greeter.sayHi("Joe");
-        printInfo(greeter);
+        System.out.println(bean.something());
+        System.out.println(bean.something());
 
         context.close();
     }
 
-    public static class Greeter {
-        public void sayHi(String name) {
-            System.out.println("Hi there, " + name);
-        }
-    }
 
     private static void printInfo(Object obj) {
         System.out.println("==========================================");
@@ -58,4 +62,5 @@ public class ConfigExample {
         System.out.println("interface name: " + Arrays.toString(obj.getClass().getInterfaces()));
         System.out.println("annotation names: " + Arrays.toString(obj.getClass().getAnnotations()));
     }
+
 }
